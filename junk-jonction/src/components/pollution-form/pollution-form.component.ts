c8533020fdd286815063d285fb8d7d599feca8be
eliott.schott <eliott.schott@pixacare.com>
2026-01-16 10:53:20 +0100

    improved pollution form

--- junk-jonction/src/components/pollution-form/pollution-form.component.ts
+++ junk-jonction/src/components/pollution-form/pollution-form.component.ts
@@ -1,11 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit } from '@angular/core';
 import { POLLUTION_FIELDS } from '../../models/config/field-config';
 import {
   FormControl,
@@ -18,18 +11,21 @@ import { latLongValidator } from '../../models/validators/latitude-longitude.val
 import { Pollution } from '../../models/types/Pollution';
 import { PollutionService } from '../../services/pollution.service';
 import { LucideAngularModule, LoaderCircle } from 'lucide-angular';
+import { first, take } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pollution-form',
-  imports: [ReactiveFormsModule, FormsModule, LucideAngularModule],
+  imports: [ReactiveFormsModule, FormsModule, LucideAngularModule, CommonModule],
   templateUrl: './pollution-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'h-full flex-1 flex-col flex items-center justify-center',
+    class:
+      'flex items-center flex-col justify-center bg-[url(/jj-bg.webp)] h-full flex-1 bg-cover bg-center ',
   },
 })
 export class PollutionFormComponent implements OnInit {
-  @Input() formTitle = 'Pollution Form';
   formGroup: FormGroup = new FormGroup({});
   pollutionTypes: string[] = ['Plastique', 'Chimique', 'Dépôt sauvage', 'Eau', 'Air', 'Autre'];
   pollutionFields = POLLUTION_FIELDS;
@@ -37,8 +33,9 @@ export class PollutionFormComponent implements OnInit {
 
   isSubmitting: boolean = false;
   message: string = '';
+  showDiv: boolean = false;
 
-  constructor(private pollutionService: PollutionService) {}
+  constructor(private pollutionService: PollutionService, private destroyRef: DestroyRef) {}
 
   ngOnInit(): void {
     this.formGroup = new FormGroup({
@@ -50,6 +47,11 @@ export class PollutionFormComponent implements OnInit {
       latitude: new FormControl('', [Validators.required, latLongValidator()]),
       longitude: new FormControl('', [Validators.required, latLongValidator()]),
       photo_url: new FormControl(''),
+    });
+
+    this.formGroup.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+      this.message = '';
+      this.isSubmitting = false;
     });
   }
 
